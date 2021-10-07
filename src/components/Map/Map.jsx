c045396bcd46f61ff033e0662d8f393ed7453106
dane <dane.m.petersen@gmail.com>
2021-10-07 11:15:45 -0500

    adding styling to map popup conditional

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Map, TileLayer, Marker, Popup, } from 'react-leaflet'
-import L, { map } from 'leaflet';
+import L from 'leaflet';
 import FullscreenControl from 'react-leaflet-fullscreen';
 import "leaflet/dist/leaflet.css";
 import { useDispatch, useSelector } from "react-redux";
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import "../Map/Map.css";
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
@@ -16,6 +17,7 @@ L.Marker.prototype.options.icon = DefaultIcon;
 
 function MapComponent({address}) {
   const dispatch = useDispatch();
+  const [showpop, setshowpop] = useState(true);
 
   useEffect(() => {
     dispatch({
@@ -26,52 +28,58 @@ function MapComponent({address}) {
 
   const dashBoard = useSelector(store => store.dashBoardReducer)
   console.log("dashboard is", dashBoard)
-  /**
-   * Map Component
-   * Utilizes react-leaflet, Leaflet
-   * Pulls precipitation and cloud data from Open Weather Map API
-   */
 
-  function getLocation() {
-    // Geocode.fromAddress(address).then(
-    //   (response) => {
-    //     const { lat, lng } = response.results[0].geometry.location;
-    //     console.log(lat, lng);
-    //     setmapaddress([lat, lng]);
-    //   },
-    //   (error) => {
-    //     console.error(error);
-    //   }
-    // );
+  function handleClick(event, name) {
+    console.log('event', event);
+    const { lat, lng } = event.latlng
+    console.log(`Clicked at ${lat}, ${lng}`)
+    console.log('name', name);
   }
 
   return (
     <>
       <h1>{address}</h1>
-      <Map center={address} zoom={14} style={{ height: "100vh" }}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <div className="map-popup-container">
+        <Map center={address} zoom={14} style={{ height: "100vh" }}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-      {dashBoard.length > 0 ? (
-        dashBoard.map((items, i) => (
-          <div key={i}>
-            <Marker position={[items.latitude, items.longitude]}>
-              <Popup>
-                <h5>{items.name}</h5>
-                <h6>{items.city}, {items.state}</h6>
-              </Popup>
-            </Marker>
+        {dashBoard.length > 0 ? (
+          dashBoard.map((items, i) => (
+            <div key={i}>
+              <Marker 
+                position={[items.latitude, items.longitude]}
+                onClick={(e) => {
+                  handleClick(e, items.name)
+                }}
+              >
+                {/* <Popup>
+                  <h5>{items.name}</h5>
+                  <h6>{items.city}, {items.state}</h6>
+                </Popup> */}
+              </Marker>
+            </div>
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+        <FullscreenControl />
+        </Map>
+        {
+          showpop === true 
+          ?
+          <div className="popup-map-item">
+            <h1>hi</h1>
+          </div> 
+          
+          :
+          <div className="popup-map-item">
+
           </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-      <FullscreenControl />
-      </Map>
-
-
+        }        
+      </div>
     </>
   );
 }
