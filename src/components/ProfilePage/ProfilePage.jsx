b94c3d6366ae932b812ec7509586a7931957621e
Farah Warfa <fwarfa@gmail.com>
2021-10-21 14:34:02 -0500

    cleanup and refactored code for profile page

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+
 export default function ProfilePage() {
-const history = useHistory();
-const user = useSelector(store => store.user);
-const editUser = useSelector(store => store.editUser);
-const dispatch = useDispatch();
-
-// import { useHistory, useParams } from 'react-router-dom';
-// const history = useHistory();
-// history.push('/home');
-
+  const history = useHistory();
+  const user = useSelector(store => store.user);
+  const editUser = useSelector(store => store.editUser);
+  const dispatch = useDispatch();
+  
+  // fetches user and saves it to a editUser reducer
   useEffect(() => {
-  dispatch({
+    dispatch({ 
+      type: 'FETCH_USER',
+    });
+    dispatch({
       type: 'SET_USER_TO_EDIT',
       payload: user
-      })
-      dispatch({ 
-        type: 'FETCH_USER',
-      });
+    })
   }, [])
 
+  // dispatching updated information to db 
+  // fetching updated info and navigating back to user page
   const userProfile = (event) => {
     event.preventDefault();
     alert('Changes Submitted');
@@ -35,6 +35,7 @@ const dispatch = useDispatch();
     history.push('/user');
     }
 
+    // updating info in reducer as user types
     const handleChange = (event) => {
       dispatch({
         type: 'UPDATE_EDIT_USER',
@@ -45,111 +46,99 @@ const dispatch = useDispatch();
       });
     }
 
-    return (
+  return (
     <>
-    <form className="card card-form formPanel" onSubmit={userProfile}>
-      <h2 onClick={user}>Profile Page</h2>
-      <div>
-        <image src={user.image}/>
-        {/* <label htmlFor="image">
-          Add Profile Picture: 
-          <input
-            type="file"
-            name="image"
-            defaultValue={user.image}
-            value={image}
-            onChange={(event) => setImage(event.target.value)}
-          />
-        </label> */}
-      </div>
-      <div >
-        <label htmlFor="first_name">
-          First name:
-          <input
-            className="form-control"
-            type="text"
-            name="first_name"
-            value={editUser.first_name}
-            required
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="last_name">
-          Last Name: 
-          <input
-            className="form-control"
-            type="text"
-            name="last_name"
-            value={editUser.last_name}
-            required
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-         <div>
-        <label htmlFor="text">
-          Email: 
-          <input
-            className="form-control"
-            type="email"
-            name="email"
-            value={editUser.email}
-            required
-            onChange={handleChange}
-          />
-        </label>
-      </div>
+      <form className="card card-form formPanel" onSubmit={userProfile}>
+        <h2 onClick={user}>Profile Page</h2>
         <div>
-        <label htmlFor="birthday">
-          Birthdate:  
-          <input
-            className="form-control"
-            type="date"
-            name="birthday"
-            value={editUser.birthday}
-            required
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="country">
-          Country: 
-          <select
-            className="form-control"
-            type="text"
-            name="country"
-            value={editUser.country}
-            required
-            onChange={handleChange}
-          >
-          {/* <option defaultValue="United States of America">United States of America</option> */}
-          <option value="United States of America">United States of America</option>
-          <option value="South America">South America</option>
-          <option value="Canada">Canada</option>
-          </select>
-        </label>
-      </div>
-      <div>
-        <label htmlFor="username">
-         Username:
-          <input
-            className="form-control"
-            type="text"
-            name="username"
-            // defaultValue={user.username}
-            value={editUser.username}
-            required
-            onChange={handleChange}
-          />
-        </label>
-      </div>
-      <div>
-        <input className="btn btn-primary" type="submit" name="submit" value="Update" />
-      </div>
-    </form>   
-      </>
-    )
+          <image src={user.image}/>
+        </div>
+        <div >
+          <label htmlFor="first_name">
+            First name:
+            <input
+              className="form-control"
+              type="text"
+              name="first_name"
+              value={editUser.first_name}
+              required
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="last_name">
+            Last Name: 
+            <input
+              className="form-control"
+              type="text"
+              name="last_name"
+              value={editUser.last_name}
+              required
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+          <div>
+          <label htmlFor="text">
+            Email: 
+            <input
+              className="form-control"
+              type="email"
+              name="email"
+              value={editUser.email}
+              required
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+          <div>
+          <label htmlFor="birthday">
+            Birthdate:  
+            <input
+              className="form-control"
+              type="date"
+              name="birthday"
+              value={editUser.birthday}
+              required
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="country">
+            Country: 
+            <select
+              className="form-control"
+              type="text"
+              name="country"
+              value={editUser.country}
+              required
+              onChange={handleChange}
+            >
+            <option value="United States of America">United States of America</option>
+            <option value="South America">South America</option>
+            <option value="Canada">Canada</option>
+            </select>
+          </label>
+        </div>
+        <div>
+          <label htmlFor="username">
+            Username:
+            <input
+              className="form-control"
+              type="text"
+              name="username"
+              value={editUser.username}
+              required
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <div>
+          <input className="btn btn-primary" type="submit" name="submit" value="Update" />
+        </div>
+      </form>   
+    </>
+  )
 }    
