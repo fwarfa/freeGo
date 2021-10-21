3bc73ff7e6a753290b602359437b21bb5c2ef55b
dane <dane.m.petersen@gmail.com>
2021-10-21 09:37:52 -0500

    cleaned up and commented register form and register page

--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-
-import { useHistory } from 'react-router-dom';
 import RegisterForm from '../RegisterForm/RegisterForm';
 
 function RegisterPage() {
-  const history = useHistory();
-
   return (
     <div>
       <RegisterForm />
